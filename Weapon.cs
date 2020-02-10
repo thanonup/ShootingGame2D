5cfc8a9df09a7d06e7d9ebf643edb9a1f5927172
thanonup <60838971+thanonup@users.noreply.github.com>
2020-02-10 18:12:54 +0700

    Add files via upload

---
+++ Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public PlayerCRL playerA;
+
+    void Update()
+    {
+        if(playerA.Notshoot == true)
+        {
+            if (Input.GetButtonDown("Fire1")) { Shoot(); playerA.Shoota(); }
+            else { playerA.Shootstop(); }
+        }     
+    }
+    public void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
